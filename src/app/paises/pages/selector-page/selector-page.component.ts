d6c30db11f531cc6bae974bda3468657247cf7c1
hguagua8517 <guaguaso9@yahoo.com>
2023-06-20 20:16:52 -0500

    Proyecto de aplicacion de paises con selectores

---
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit{
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required]
+  })
+
+  //llenar selectores
+  regiones: string[] = [];
+
+  constructor (private fb: FormBuilder,
+    private paisesService: PaisesService){}
+
+  ngOnInit(): void {
+    this.regiones = this.paisesService.regiones;
+  }
+
+  guardar(){
+    this.miFormulario.value;
+    console.log(this.miFormulario.value);  }
+
+}
